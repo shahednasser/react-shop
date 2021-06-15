129d2ac65288a207a232380e0bb94ca8db191ee9
Shahed Nasser <shahednasser@gmail.com>
2021-06-15 18:23:57 +0300

    remove unnecessary classes

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,7 @@
 function Product ({product: {title, price, description, category, image}}) {
   return (
     <div className="flex bg-white w-full mb-5 shadow-sm rounded-lg dark:bg-gray-300 group overflow-hidden hover:shadow-2xl transition-shadow duration-300">
-      <div className="w-5/12 p-2 dark:bg-white rounded-tl-lg rounded-bl-lg">
+      <div className="w-5/12 p-2 dark:bg-white">
         <div style={{backgroundImage: `url(${image})`}} className="bg-contain bg-no-repeat bg-center w-full h-full transition-transform duration-300 group-hover:transform group-hover:scale-125"></div>
       </div>
       <div className="w-7/12 p-5">
